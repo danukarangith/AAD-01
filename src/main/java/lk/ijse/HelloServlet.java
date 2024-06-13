48744195c9c6d69749de72a993fabd054ce5b8cc
danukarangith <danukarangith456@gmail.com>
2024-06-13 11:36:09 +0530

    initial commit

---
+++ src/main/java/lk/ijse/HelloServlet.java
@@ -0,0 +1,15 @@
+package lk.ijse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("incoming request" +req.getRemoteAddr());
+        resp.getWriter().println("Hello ,Im from hello servlet");
+    }
+}
